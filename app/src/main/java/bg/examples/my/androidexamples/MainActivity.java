6f029ac1d708676050d2db13717ff8be09c6a1e9
Stoyan <stoyan.p12@gmail.com>
2016-09-20 15:33:52 +0300

    RoundedButtons and background texture

--- app/src/main/java/bg/examples/my/androidexamples/MainActivity.java
+++ app/src/main/java/bg/examples/my/androidexamples/MainActivity.java
@@ -1,15 +1,20 @@
 package bg.examples.my.androidexamples;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.Point;
 import android.graphics.PointF;
 import android.support.annotation.StringDef;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.Touch;
+import android.util.DisplayMetrics;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +24,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Context context = getApplicationContext();
+        String language_code = "bg";
+
+        Resources res = context.getResources();
+        // Change locale settings in the app.
+        DisplayMetrics dm = res.getDisplayMetrics();
+        android.content.res.Configuration conf = res.getConfiguration();
+        conf.locale = new Locale(language_code.toLowerCase());
+        res.updateConfiguration(conf, dm);
+
         setContentView(R.layout.activity_main);
 
         btnPop = (Button)findViewById(R.id.buttonPop);
---
+++ app/src/main/java/bg/examples/my/androidexamples/RoundedButtons.java
@@ -0,0 +1,13 @@
+package bg.examples.my.androidexamples;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RoundedButtons extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rounded_buttons);
+    }
+}
